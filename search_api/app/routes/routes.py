5672daf56ca9fbe6dcbab0d58f1b193829a17b75
josh <jh@semetrical.com>
2022-05-30 19:50:38 +0100

    Added loading icon in app when querying search results. Load times can get quite long!

--- search_api/app/routes/routes.py
+++ search_api/app/routes/routes.py
@@ -38,13 +38,14 @@ def search_paper_metadata():  # put application's code here
     if search_term is None:
         return jsonify([])
 
-    # hard limit in place to prevent broad queries from fetching entire database
-    # pagination is a clear solution to this. Likely offset+limit pagination.
+    # hard limit has been set in place to prevent broad queries from fetching entire database and slowing down the react app
+    # pagination is a clear solution to this.Smaller pages=smaller data structures=no slowdown. Likely offset+limit pagination.
     # offset + limit pagination can lead to performance slow-downs server side
     # However performance hit from returning 40k records seems to occur within the browser itself when loading + rendering data
-    # So I'm not too concerned about server speed limits Offset + limit pagination is easy to implement so it wins as the choice of pagination approach
+    # So I'm not too concerned about server speed limits incurred from this pagination choice
+    # Offset + limit pagination is also easy to implement so it wins as the choice of pagination approach
 
-    # Where this production code. Pagination would be the next API feature to add!!
+    # Where this production code. Pagination would be the next (and critial) API feature to add!!
     results = PaperStore.query.filter(PaperStore.title.like(f"%{search_term}%") |
                                       PaperStore.authors.like(f"%{search_term}%") |
                                       PaperStore.journal.like(f"%{search_term}%") |
